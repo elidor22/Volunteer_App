7a8e79da4bb512218dd5fb498f22c770c3fcc860
elidor22 <elidorv@outlook.com>
2020-05-19 22:31:44 +0200

    Implemented fully functional scalable uploading

--- volunteer_app/app/Http/Controllers/uploadController.php
+++ volunteer_app/app/Http/Controllers/uploadController.php
@@ -9,11 +9,21 @@ class uploadController extends Controller
 {
     public function store(Request $request)
     {
-        $file = $request->photo;
-
-        if ($request->hasFile('photo')) {
-            return 201;
+        // Handle File Upload
+        if($request->hasFile('cover_image')){
+            // Get filename with the extension
+            $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
+            // Get just filename
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            // Get just ext
+            $extension = $request->file('cover_image')->getClientOriginalExtension();
+            // Filename to store
+            $fileNameToStore= $filename.'_'.time().'.'.$extension;
+            // Upload Image
+            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+        } else {
+            $fileNameToStore = 'noimage.jpg';
         }
-        return 'success';
-    }
+        return $fileNameToStore;
+    } 
 }
